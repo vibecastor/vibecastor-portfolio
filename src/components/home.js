e471c19de2f92f5a476577d802def17604dac627
vibecastor <vibecastor@gmail.com>
2018-09-24 09:36:13 -0700

    adding static image queries to projects component

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,35 @@
 import React from 'react';
 import Img from 'gatsby-image';
 import { StaticQuery, graphql } from 'gatsby';
+import styled from 'styled-components';
+
+const HomeContainer = styled.div`
+  position: relative;
+  margin: 0 auto;
+  z-index: 0;
+  text-align: center;
+`;
+
+const Banner = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  h1 {
+      color: yellow; 
+  }
+  h2 {
+      color: yellow;
+     }
+`;
+
+const scrollDown = (event, section) => {
+  event.preventDefault();
+  document.getElementById(section).scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
+};
 
 const Home = () => (
  <StaticQuery
@@ -21,12 +50,20 @@ const Home = () => (
       }
     `}
     render={data => (
+      <HomeContainer>
       <Img
         fluid={data.file.childImageSharp.fluid}
         style={{
           width: '100%',
         }}
       />
+        <Banner>
+          <h1>Hello, I'm Mike,</h1>
+          <h2>a creative, full-stack dev in Seattle!</h2>
+
+        <button href='#about' onClick={event => scrollDown(event, 'about')}>click me!</button>
+        </Banner>
+      </HomeContainer>
     )}
  />
 );
--- src/components/projects.js
+++ src/components/projects.js
@@ -20,9 +20,23 @@ const Projects = () => (
             title
           }
         }
-        file(relativePath: { regex: "/eventus-image.png/" }) {
+        imageOne: file(relativePath: { regex: "/eventus-image.png/" }) {
           childImageSharp {
-            fluid(maxWidth: 1240) {
+            fluid(maxWidth: 1000) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        imageTwo: file(relativePath: { regex: "/phrasecraze-image.png/" }) {
+          childImageSharp {
+            fluid(maxWidth: 1000) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        imageThree: file(relativePath: { regex: "/plenty-of-dogs-image.png/" }) {
+          childImageSharp {
+            fluid(maxWidth: 1000) {
               ...GatsbyImageSharpFluid
             }
           }
@@ -32,49 +46,31 @@ const Projects = () => (
     render={data => (
       <ProjectsWrapper id='projects'>
         <h1>Projects</h1>
+        <h2>Event Us</h2>
       <Img
-        fluid={data.file.childImageSharp.fluid}
+        fluid={data.imageOne.childImageSharp.fluid}
         style={{
-          width: '100%',
+          width: '70%',
         }}
       />
+        <h2>Phrasecraze</h2>
+        <Img
+          fluid={data.imageTwo.childImageSharp.fluid}
+          style={{
+            width: '70%',
+          }}
+        />
+        <h2>Plenty of Dogs</h2>
+        <Img
+          fluid={data.imageThree.childImageSharp.fluid}
+          style={{
+            width: '70%',
+          }}
+        />
         <ScrollHome />
       </ProjectsWrapper>
     )}
   />
 );
-
-  {/*<ProjectsWrapper className='projects'>*/}
-    {/*<h1>Projects</h1>*/}
-    {/*<div className='project-container'>*/}
-      {/*<ul>*/}
-        {/*<li className='card'>*/}
-          {/*<h1>EventUs</h1>*/}
-          {/*<a href='http://eventus.site'>*/}
-            {/*<img src={EventUs} alt='eventus.site' height='80%' width='80%'></img>*/}
-          {/*</a>*/}
-          {/*<p>EventUs is a social event creation and discovery application.*/}
-            {/*A user can sign up, create a profile, create an event and view a list of events in an event feed on the main page view.*/}
-            {/*This application was built for a social running club called Hash House Harriers.  The application is built using the MERN Stack (MongoDB, Express, React/Redux and Node).</p>*/}
-        {/*</li>*/}
-        {/*<li className='card'>*/}
-          {/*<h1>Phrasecraze</h1>*/}
-          {/*<a href='https://github.com/spinaltaper/phrasecraze'>*/}
-            {/*/!*<img src={Phrasecraze} alt='phrasecraze.website' height='80%' width='80%'></img>*!/*/}
-          {/*</a>*/}
-          {/*<p>PhaseCraze is a word game REST API that consists of both a TCP server and HTTP server.  The TCP server is hosted on a Digital Ocean Droplet and the HTTP server is hosted on Heroku.  This project only runs on the command line.  Phrasecraze was built with Node.js, Express and MongoDB.</p>*/}
-        {/*</li>*/}
-        {/*<li className='card'>*/}
-          {/*<h1>Plenty of Dogs</h1>*/}
-          {/*<a href='http://plenty-of-dogs.site'>*/}
-            {/*/!*<img src={PlentyOfDogs} alt='plenty-of-dogs.site' height='80%' width='80%'></img>*!/*/}
-          {/*</a>*/}
-          {/*<p>Plenty of Dogs is a client side application where a user can take a quiz to discover their preferred dog for adoption.  The quiz utilizes data from an API request to dog.ceo which is a public API of dog images.  The application then takes the results of the users quiz and makes a call to the Petfinder API which contains data on dogs that are currently available for adoption.  Plenty of Dogs was built with vanilla ES5 javascript, jQuery, Express and PostgreSQL.</p>*/}
-        {/*</li>*/}
-      {/*</ul>*/}
-    {/*</div>*/}
-  {/*</ProjectsWrapper>*/}
-
-{/*);*/}
 
 export default Projects;
